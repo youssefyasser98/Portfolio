bce9662b3c7fda5ac3d757973d73f26ec8c07b3c
youssefyasser98 <80383371+youssefyasser98@users.noreply.github.com>
2022-04-20 19:24:08 +0200

    Update Info.js

--- src/data/Info.js
+++ src/data/Info.js
@@ -30,9 +30,9 @@ export const AboutInfo = () => {
         <br />
         <br />
         At last, Feel free to{" "}
-         <a style={style}>
+        
           Contact ME.
-         </a>{" "}
+         {" "}
        
       </p>
     </>
